2291833c1d2d6875869384efd1b07d9407cb0f22
Anas Derk <anas.derk.2020@gmail.com>
2022-08-09 03:33:45 -0400

    Config The Project For Deploy

--- models/DB_URL.js
+++ models/DB_URL.js
@@ -1,4 +1,4 @@
-// const DB_URL = "mongodb+srv://anas:a1n2a3s4@online-store.8ns4h.mongodb.net/online-store?retryWrites=true&w=majority"
-const DB_URL = "mongodb://localhost:27017/online-shop";
+const DB_URL = "mongodb+srv://anas:a1n2a3s4@online-store.mtzsq9b.mongodb.net/?retryWrites=true&w=majority"
+// const DB_URL = "mongodb://localhost:27017/online-shop";
 
 module.exports = DB_URL;
